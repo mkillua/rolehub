3bcdeb21f76112d0f4132d1fdb01f8a2d64ea300
Mauricio Rodrigues <mauricio.reborn@hotmail.com>
2023-08-11 14:28:50 -0300

    add correct url

--- src/requests/urls/api.js
+++ src/requests/urls/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const token = localStorage.getItem('access_token');
 
 const api = axios.create({
- baseURL: "http://localhost:8000/api/",
+ baseURL: "rolehub-api.up.railway.app/api/",
  headers: {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer ' + token
